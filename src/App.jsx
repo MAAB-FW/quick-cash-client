30072f8136d80deae81678706fab9dfb586b50e4
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-07-15 20:41:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+    return (
+        <>
+            <div className="text-center">
+                <h2 className="text-2xl">This is Template Project</h2>
+                <Button className="mx-5">Hello</Button>
+            </div>
+        </>
+    )
+}
+
+export default App
