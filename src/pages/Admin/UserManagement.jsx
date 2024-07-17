dd0111191b60c4a5f3b7fa6c39228694f251c63a
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-07-17 13:32:58 +0600

    update: userManagement page are now fully dynamic

--- src/pages/Admin/UserManagement.jsx
+++ src/pages/Admin/UserManagement.jsx
@@ -1,32 +1,43 @@
-import LoadingSpinner from "@/components/LoadingSpinner";
 import useAxiosSecure from "@/hooks/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import React, { useId, useState } from "react";
 import { RingLoader } from "react-spinners";
 
 const UserManagement = () => {
     const axiosSecure = useAxiosSecure();
     const [searchText, setSearchText] = useState("");
-    const { data: users, isPending } = useQuery({
+    const [isRefetching, setIsRefetching] = useState(false);
+    const {
+        data: users,
+        isPending,
+        refetch,
+    } = useQuery({
         queryKey: ["users", searchText],
         queryFn: async () => {
             const res = await axiosSecure.get(`/users?search=${searchText}`);
             // console.log(res.data);
+            setIsRefetching(false);
             return res.data;
         },
     });
-    // const [users, setUsers] = useState(users);
+    const { mutateAsync } = useMutation({
+        mutationKey: ["status"],
+        mutationFn: async ({ userId, status, button }) => {
+            const res = await axiosSecure.patch(`/userStatus/${userId}`, { status, button });
+            console.log(res.data);
+            if (res.data.modifiedCount) {
+                refetch();
+            }
+        },
+    });
 
-    const handleToggleStatus = (userId) => {
-        console.log(userId);
+    const handleToggleStatus = (userId, status, button) => {
+        setIsRefetching(true);
+        mutateAsync({ userId, status, button });
     };
 
-    // if (isPending) {
-    //     return <LoadingSpinner />;
-    // }
-
     return (
-        <div className="min-h-[50vh] p-4">
+        <div className="min-h-[50vh] p-4 relative">
             <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-6">
                 <h2 className="text-2xl font-bold mb-4 text-gray-800">User Management</h2>
                 <div className="mb-4">
@@ -51,46 +62,71 @@ const UserManagement = () => {
                         </thead>
                         <tbody>
                             {isPending ? (
-                                <td colSpan="4">
-                                    <div className="flex justify-center items-center h-[50vh]">
-                                        <RingLoader color="#2a3cb3" size={100}></RingLoader>
-                                    </div>
-                                </td>
+                                <tr>
+                                    <td colSpan="4">
+                                        <div className="flex justify-center items-center h-[50vh]">
+                                            <RingLoader color="#2a3cb3" size={100}></RingLoader>
+                                        </div>
+                                    </td>
+                                </tr>
                             ) : users?.length < 1 ? (
-                                <td colSpan="4">
-                                    <div className="flex justify-center items-center h-[50vh]">Empty</div>
-                                </td>
+                                <tr>
+                                    <td colSpan="4">
+                                        <div className="flex justify-center items-center h-[50vh]">Empty</div>
+                                    </td>
+                                </tr>
                             ) : (
                                 users?.map((user) => (
                                     <tr key={user._id} className="*:text-center *:min-w-32">
                                         <td className="py-2 px-4 border-b">{user.name}</td>
                                         <td className="py-2 px-4 border-b">{user.email}</td>
                                         <td className="py-2 px-4 border-b">
-                                            <span
-                                                className={`py-1 px-3 rounded-full text-white ${
-                                                    user.status === "approved" ? "bg-green-500" : "bg-red-500"
-                                                }`}
-                                            >
-                                                {user.status}
-                                            </span>
+                                            {user.role === "admin" ? (
+                                                <span className="py-1 px-3 rounded-full font-bold bg-blue-600 text-white">
+                                                    Admin
+                                                </span>
+                                            ) : (
+                                                <span
+                                                    className={`py-1 px-3 rounded-full text-white font-semibold ${
+                                                        user.status === "approved" ? "bg-green-500" : "bg-red-500"
+                                                    }`}
+                                                >
+                                                    {user.status.slice(0, 1).toUpperCase() + user.status.slice(1)}
+                                                </span>
+                                            )}
                                         </td>
                                         <td className="py-2 px-4 border-b">
-                                            <button
-                                                onClick={() => handleToggleStatus(user.id)}
-                                                className={`py-1 px-3 rounded-lg ${
-                                                    user.status === "approved"
-                                                        ? "bg-red-500 text-white"
-                                                        : "bg-green-500 text-white"
-                                                }`}
-                                            >
-                                                {user.status === "approved" ? "Block" : "Activate"}
-                                            </button>
+                                            {user.role === "admin" ? (
+                                                <></>
+                                            ) : (
+                                                <div className="flex flex-col gap-2">
+                                                    <button
+                                                        disabled={user.status === "approved"}
+                                                        onClick={() => handleToggleStatus(user._id, user.status, "approved")}
+                                                        className={`py-1 px-3 rounded-lg font-semibold text-white bg-green-500 disabled:bg-gray-400 disabled:text-gray-100 disabled:cursor-not-allowed`}
+                                                    >
+                                                        {user.status === "pending" ? "Approve" : "Activate"}
+                                                    </button>
+                                                    <button
+                                                        disabled={user.status === "rejected"}
+                                                        onClick={() => handleToggleStatus(user._id, user.status, "rejected")}
+                                                        className={`py-1 px-3 rounded-lg font-semibold text-white bg-red-500 disabled:bg-gray-400 disabled:text-gray-100 disabled:cursor-not-allowed`}
+                                                    >
+                                                        {user.status === "pending" ? "Reject" : "Block"}
+                                                    </button>
+                                                </div>
+                                            )}
                                         </td>
                                     </tr>
                                 ))
                             )}
                         </tbody>
                     </table>
+                    {isRefetching && (
+                        <div className="flex top-1/2 left-1/3  absolute justify-center items-center ">
+                            <RingLoader color="#2a3cb3" size={100}></RingLoader>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
