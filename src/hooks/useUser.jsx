4b8ec6385631ac91e63163e19eb0ee36d7ee8882
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-07-25 11:54:43 +0600

    update: Transaction History page for agent are now dynamic

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -4,12 +4,16 @@ import useAxiosSecure from "./useAxiosSecure";
 const useUser = () => {
     const axiosSecure = useAxiosSecure();
 
-    const { data: user, isPending,refetch } = useQuery({
+    const {
+        data: user,
+        isPending,
+        refetch,
+    } = useQuery({
         queryKey: ["user"],
         // enabled: !loading && !!user,
         queryFn: async () => {
-            const res = await axiosSecure.get(`/userInfo/`);
-            // console.log(res.data.role)
+            const res = await axiosSecure.get(`/userInfo`);
+            // console.log(res.data.role);
             return res.data;
         },
     });
--- src/pages/Agent/TransactionsHistoryAgent.jsx
+++ src/pages/Agent/TransactionsHistoryAgent.jsx
@@ -1,19 +1,24 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { TbCurrencyTaka } from "react-icons/tb";
 
 const TransactionsHistoryAgent = () => {
-    const transactions = [
-        { id: 1, type: "Cash In", mobile: "0123456789", amount: -1000, fee: 0, time: "2024-07-01 12:00:00" },
-        { id: 2, type: "Cash Out", mobile: "0123456789", amount: 300, fee: 4.5, time: "2024-07-03 16:00:00" },
-        { id: 3, type: "Cash In", mobile: "0123456789", amount: -200, fee: 0, time: "2024-07-02 14:30:00" },
-        { id: 4, type: "Cash Out", mobile: "0123456789", amount: 200, fee: 3.0, time: "2024-07-04 09:00:00" },
-        { id: 5, type: "Cash In", mobile: "0123456789", amount: -500, fee: 0, time: "2024-07-05 11:15:00" },
-        { id: 6, type: "Cash In", mobile: "0123456789", amount: -50, fee: 0, time: "2024-07-06 13:45:00" },
-        { id: 7, type: "Cash Out", mobile: "0123456789", amount: 100, fee: 1.5, time: "2024-07-07 10:30:00" },
-        { id: 8, type: "Cash Out", mobile: "0123456789", amount: 400, fee: 6.0, time: "2024-07-08 15:00:00" },
-        { id: 9, type: "Cash In", mobile: "0123456789", amount: -700, fee: 0, time: "2024-07-09 17:45:00" },
-        { id: 10, type: "Cash In", mobile: "0123456789", amount: -100, fee: 0, time: "2024-07-10 19:30:00" },
-    ];
+    const axiosSecure = useAxiosSecure();
+
+    const { data: transactions = [], isPending } = useQuery({
+        queryKey: ["historyAgent"],
+        // enabled: !loading && !!user,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/historyAgent`);
+            // console.log(res.data.role)
+            return res.data;
+        },
+    });
+    if (isPending) {
+        return <LoadingSpinner></LoadingSpinner>;
+    }
     return (
         <div className="">
             <div className="mx-auto bg-white shadow-lg rounded-lg p-6">
@@ -22,7 +27,7 @@ const TransactionsHistoryAgent = () => {
                     <table className="min-w-full bg-white text-nowrap">
                         <thead>
                             <tr>
-                                <th className="py-2">Transaction ID</th>
+                                <th className="py-2">Transaction _ID</th>
                                 <th className="py-2">Type</th>
                                 <th className="py-2">Mobile Number</th>
                                 <th className="py-2">Amount</th>
@@ -32,21 +37,21 @@ const TransactionsHistoryAgent = () => {
                         </thead>
                         <tbody>
                             {transactions.map((transaction) => (
-                                <tr key={transaction.id} className="bg-gray-100 text-center *:min-w-36">
-                                    <td className="border px-4 py-2">{transaction.id}</td>
+                                <tr key={transaction._id} className="bg-gray-100 text-center *:min-w-fit">
+                                    <td className="border px-4 py-2">{transaction._id}</td>
                                     <td className="border px-4 py-2">{transaction.type}</td>
-                                    <td className="border px-4 py-2">{transaction.mobile}</td>
+                                    <td className="border px-4 py-2">{transaction.userInfo.phone}</td>
                                     <td
-                                        className={`border px-4 py-2 flex items-center ${
-                                            transaction.amount < 0 ? "text-red-500" : "text-green-500"
+                                        className={`border px-4 py-2 flex items-center justify-end ${
+                                            transaction.type === "Cash In" ? "text-red-500" : "text-green-500"
                                         }`}
                                     >
-                                        {transaction.amount < 0 ? "-" : "+"}
+                                        {transaction.type === "Cash In" ? "-" : "+"}
                                         <TbCurrencyTaka />
-                                        {Math.abs(transaction.amount).toFixed(2)}
+                                        {Math.abs(transaction.amount)?.toFixed(2)}
                                     </td>
-                                    <td className="border px-4 py-2">{transaction.fee.toFixed(2)}</td>
-                                    <td className="border px-4 py-2">{transaction.time}</td>
+                                    <td className="border px-4 py-2">{transaction.fee ? transaction.fee?.toFixed(2) : <>-</>}</td>
+                                    <td className="border px-4 py-2">{new Date(transaction.time).toLocaleString()}</td>
                                 </tr>
                             ))}
                         </tbody>
